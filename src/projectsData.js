2af84db8620e5ef3581169dce682e50a1c5cefb4
Jose <Jose0156.114@gmail.com>
2024-12-18 15:25:33 -0600

    agregando proyecto

--- src/projectsData.js
+++ src/projectsData.js
@@ -13,16 +13,16 @@ export const projects = [
       "https://storage.screenshotapi.net/ticket_system_37ac_onrender_com__9b2a80f0bea3.png",
   },{
     id: 1,
-    title: "Ticket System",
+    title: "Snake Game",
     description: {
-      es: "Sistema de gestión de tickets CRUD con implementacion de guardado en Base de Datos NoSQL MongoDB Atlas.",
-      en: "CRUD ticket management system with implementation of saving in MongoDB Atlas NoSQL Database.",
+      es: "Juego de La serpiente en TypesScript con HTML5 Canvas CSS3 y Node.js.",
+      en: "Snake game in TypesScript with HTML5 Canvas CSS3 and Node.js.",
     },
-    githubLink: "https://github.com/Jose-1204/ticket-system",
-    renderLink: "https://ticket-system-37ac.onrender.com",
-    techStack: ["JavaScript", "Node.js", "HTML", "CSS", "MongoDB"],
+    githubLink: "https://github.com/Jose-1204/snake-game",
+    renderLink: "",
+    techStack: ["TypeScript", "JavaScript", "Node.js", "HTML5", "CSS3"],
     image:
-      "https://storage.screenshotapi.net/ticket_system_37ac_onrender_com__9b2a80f0bea3.png",
+      "insertar img",
   },
   {
     id: 1,
